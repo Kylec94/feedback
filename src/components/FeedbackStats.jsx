c5f269381c8f6f871b86ef56f0b5ff18051030a3
Kylec94 <kyleianc94@gmail.com>
2022-05-20 11:43:21 -0600

    update

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
+
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext);
+  let average =
+    feedback.reduce((acc, curr) => {
+      return acc + curr.rating;
+    }, 0) / feedback.length;
+
+  average = average.toFixed(1).replace(/[.,]0$/, ""); //toFixed(1) makes it so theres only one decimal (9.0, 9.5, etc) and .replace(gibberish) makes it so if theres a 0 then it wont show.
+
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
+  );
+}
+
+export default FeedbackStats;
+
+//average rating...if average is not a number, return 0, else return average
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+
+function Header({ text }) {
+  return (
+    <header style={{ backgroundColor: "blue", color: "red" }}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
